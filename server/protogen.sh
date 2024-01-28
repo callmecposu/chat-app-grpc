31bab708b6813aed03653c7f56d055072104eac5
callmecposu <pc.cposu@gmail.com>
2024-01-28 17:05:33 -0500

    #client: setup the client

--- server/protogen.sh
+++ server/protogen.sh
@@ -4,3 +4,10 @@ npx grpc_tools_node_protoc \
 --grpc_out=./protos/out \
 --proto_path=./protos/src \
 ./protos/src/chat/chat.proto
+# clear the src protos in the client folder
+rm -r ../client/src/protos/src
+# copy the proto source files to the client folder
+cp -r ./protos/src ../client/src/protos
+# run the client protogen script
+cd ../client
+./protogen.sh
